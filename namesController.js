d38031ea8b09757947dfbadaea0ef0dda5ae6eac
Shuyu Zhang <sz3160@att.com>
2015-08-04 09:39:30 -0500

    feat(commit angularRepeatOrder and index)

---
+++ namesController.js
@@ -0,0 +1,24 @@
+angular.module('myApp2', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
+
+angular.module('myApp', [])
+
+.controller('customersCtrl', function($scope) {
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.searchFish   = '';     // set the default search/filter term
+  
+  // create the list of sushi rolls 
+  $scope.sushi = [
+    { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
+    { name: 'Philly', fish: 'Tuna', tastiness: 4 },
+    { name: 'Tiger', fish: 'Eel', tastiness: 7 },
+    { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+  ];
+  
+});
